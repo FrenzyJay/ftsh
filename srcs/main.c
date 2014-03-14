58c1d6122299d4375eaca9176809413d754e73fc
ato <ato@atonium.fr>
2014-03-15 00:26:41 +0100

    New common lib

--- srcs/main.c
+++ srcs/main.c
@@ -1,25 +1,13 @@
 #include <unistd.h>
 #include "42sh.h"
 
-int	ft_error(char *msg)
-{
-	int		i;
-
-	i = 0;
-	while (msg + i)
-		i++;
-	write(2, msg, i);
-	return (-1);
-}
-
 int main(int argc, char **argv)
 {
 	char	*line;
 
 	if (argc > 1)
 		return (ft_error(argv[0]));
-	while (get_next_line(0, &line) >= 0)
-		ft_printf("entry : %s\n", line);
+	ft_putendl("~~ Hum ಠ_ಠ ~~\n");
 	return (0);
 }
 
