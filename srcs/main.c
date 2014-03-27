6575585552abf643a09f69e79d8195d77d7382de
Guillaume ARM <garm@e1r6p11.42.fr>
2014-03-27 09:54:07 +0100

    fix bug in parser_error.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jvincent <jvincent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/17 16:39:20 by jvincent          #+#    #+#             */
-/*   Updated: 2014/03/27 05:43:59 by garm             ###   ########.fr       */
+/*   Updated: 2014/03/27 09:27:45 by garm             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,12 @@
 int		main(int argc, char **argv)
 {
 	char	*entry;
-	char	*error;
 	t_node	*ast;
 
 	while (get_next_line(0, &entry))
 	{
 		ast = ft_parser(entry);
-		if ((error = ft_parser_check_error(ast)))
-			ft_printf("ftsh: parse error near '%s'\n", error);
-		else
-			ft_put_ast(ast);
+		ft_put_ast(ast);
 		ft_ast_destroy(&ast);
 	}
 
